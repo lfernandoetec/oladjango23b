244bf4bac46cce1de25febaeb55c9d2588077df1
lfernandoetec <lfernandoetec@gmail.com>
2023-05-19 11:04:37 -0300

    index model

--- itapira/enquete/urls.py
+++ itapira/enquete/urls.py
@@ -6,4 +6,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('loucura', views.loucura, name='loucura'),
+    # ex: /enquete/5/
+    path("<int:questao_id>/", views.detalhe, name="detalhe"),
+    # ex: /enquete/5/resultados/
+    path("<int:questao_id>/resultados/", views.resultados, name="resultados"),
+    # ex: /enquete/5/voto/
+    path("<int:questao_id>/voto/", views.voto, name="voto"),
 ]
--- itapira/enquete/views.py
+++ itapira/enquete/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Questao
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Cheia de mania, toda dengosa, digui digui ieeee")
+    ultimas_questoes = Questao.objects.order_by("-data")[:5]
+    saida = ", ".join([q.pergunta for q in ultimas_questoes])
+    return HttpResponse(saida)
 
 def loucura(request):
     return HttpResponse("E nessa loucura.......")
+
+def detalhe(request, questao_id):
+    return HttpResponse("Voce esta olhando a questao %s." % questao_id)
+
+
+def resultados(request, questao_id):
+    response = "Voce esta olhando o resultado da questao %s."
+    return HttpResponse(response % questao_id)
+
+
+def voto(request, questao_id):
+    return HttpResponse("Voce esta votando na questao %s." % questao_id)
