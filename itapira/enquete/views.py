1c7f0ec9d80cfa3ec93fc571d54dc62940c1d955
lfernandoetec <lfernandoetec@gmail.com>
2023-08-04 10:24:50 -0300

    views genericas

--- itapira/enquete/views.py
+++ itapira/enquete/views.py
@@ -1,29 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 from .models import Questao, Resposta
 
 # Create your views here.
-def index(request):
-    ultimas_questoes = Questao.objects.order_by("-data")#[:5]
-    context = {'ultimas_questoes': ultimas_questoes}
-    return render(request, 'enquete/index.html', context)
-
-def loucura(request):
-    return HttpResponse("E nessa loucura.......")
-
-def detalhe(request, questao_id):
-    # try:
-    #     questao = Questao.objects.get(pk=questao_id)
-    # except Questao.DoesNotExist:
-    #     raise Http404("Questao naum ecxisty")
-    questao = get_object_or_404(Questao, pk=questao_id)
-    return render(request, 'enquete/questao.html', {'questao': questao})
+class IndexView(generic.ListView):
+    model = Questao
+    template_name = 'enquete/index.html'
 
 
-def resultados(request, questao_id):
-    questao = get_object_or_404(Questao, pk=questao_id)
-    return render(request, 'enquete/resultado.html', {'questao': questao})
+class DetalheView(generic.DetailView):
+    model = Questao
+    template_name = 'enquete/questao.html'
+
+
+class ResultadosView(generic.DetailView):
+    model = Questao
+    template_name = 'enquete/resultado.html'
 
 
 def voto(request, questao_id):
